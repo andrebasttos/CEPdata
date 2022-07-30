af51366ed4dd744e981b5202339547d004878ace
Igor Iglesias <contato@igoriglesias.com>
2022-07-30 17:42:03 -0300

    Melhoria na validação e tratamento de erros

---
+++ cep3.py
@@ -0,0 +1,58 @@
+import re
+import requests
+
+def prepare_cep(cep: str):
+    _cep = re.findall('[0-9]+', cep)
+    _cep = _cep and "".join(_cep)
+    return _cep
+
+def is_valid(cep):
+    return bool(len(cep) == 8)
+
+def call_viacep(cep):
+
+    try:
+        response = requests.get(f"http://www.viacep.com.br/ws/{cep}/json/", timeout=10)
+
+        if not response.status_code == 200 or 'erro' in response.json():
+            return {'error': 'Cep não encontrato'}
+        return response.json()
+
+    except Exception as e:
+        return {'error': e} 
+
+def handle_cep(cep: str):
+    
+    cep = prepare_cep(cep)
+
+    if is_valid(cep):
+
+        endereco = call_viacep(cep)
+        if not 'error' in endereco:
+            print("\n",endereco)
+        else:
+            print("\n",endereco['error'])
+
+    else:
+        print("Errouu!")
+
+
+def main():
+    continua = True
+    while continua:
+        print("""
+            Digite um CEP válido ou X para sair
+        """)
+
+        cep = input(": ")
+        if cep == 'x' or cep == 'X':
+            # Poderia ser apenas um break
+            continua = False
+            continue
+
+        handle_cep(cep)
+
+
+if "__main__" == __name__:
+    main()
+    
